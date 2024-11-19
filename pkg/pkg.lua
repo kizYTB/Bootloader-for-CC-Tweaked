fe39863e7744bfc389d014b8edd2ce474abce1f5
kiz___ <140178579+kizYTB@users.noreply.github.com>
2024-11-19 20:15:23 +0100

    Update pkg.lua

--- pkg/pkg.lua
+++ pkg/pkg.lua
@@ -1,7 +1,7 @@
 run=shell.run
 
 run("mkdir","/.bootloader/")
-run("cd","/.bootloader/")"cd /.bootloader/",
+run("cd","/.bootloader/")
 run("wget","https://raw.githubusercontent.com/kizYTB/Bootloader-for-CC-Tweaked/refs/heads/main/boot.lua")
 run("mkdir","OS/ Logo/ DefaultOS/")
 run("cd","OS/")
