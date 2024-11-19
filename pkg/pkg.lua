7cf95823363f4dfae945af5f75c5e61f3ef4623e
kiz___ <140178579+kizYTB@users.noreply.github.com>
2024-11-19 20:14:32 +0100

    Update pkg.lua

--- pkg/pkg.lua
+++ pkg/pkg.lua
@@ -1,1 +1,14 @@
+run=shell.run
 
+run("mkdir","/.bootloader/")
+run("cd","/.bootloader/")"cd /.bootloader/",
+run("wget","https://raw.githubusercontent.com/kizYTB/Bootloader-for-CC-Tweaked/refs/heads/main/boot.lua")
+run("mkdir","OS/ Logo/ DefaultOS/")
+run("cd","OS/")
+run("wget","https://raw.githubusercontent.com/kizYTB/Bootloader-for-CC-Tweaked/refs/heads/main/OS/CraftOS.json")",
+run("cd","../Logo")
+run("wget","https://raw.githubusercontent.com/kizYTB/Bootloader-for-CC-Tweaked/refs/heads/main/Logo/logo.nfp")
+run("cd","./DefaultOS")
+run("wget","https://raw.githubusercontent.com/kizYTB/Bootloader-for-CC-Tweaked/refs/heads/main/DefaultOS/boot.lua")
+run("cd","/")
+run("wget","https://raw.githubusercontent.com/kizYTB/Bootloader-for-CC-Tweaked/refs/heads/main/startup.lua")
